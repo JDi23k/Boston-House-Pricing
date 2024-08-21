783f9e864b8b45b5220d42ae9d28936159e3427c
JDi23k <jdi023k@gmail.com>
2024-08-21 12:53:11 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ scalar=pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
@@ -23,13 +23,13 @@ def predict_api():
     print(result[0])
     return jsonify(result[0])
 
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     data=[float(x) for x in request.form.values()]
-#     final_input=scalar.transform(np.array(data).reshape(1,-1))
-#     print(final_input)
-#     output=regmodel.predict(final_input)[0]
-#     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("index.html",prediction_text="The House price prediction is {}".format(output))
 
 
 if __name__=="__main__":
